c8301b5ce73144919f0715bfb90bc1b6f800dc33
Adrien Gallouët <adrien@gallouet.fr>
2026-06-05 16:04:38 +0200

    Align cuda probe with the real binary (#18)
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- cuda/probe.c
+++ cuda/probe.c
@@ -1,44 +1,53 @@
 #include <stdio.h>
-#include <cuda_runtime.h>
+#include <cuda.h>
 
 #ifndef PROBE_ARCH
 #error "Please define PROBE_ARCH"
 #endif
 
+#ifdef __linux__
+#include <sys/epoll.h>
+#endif
+
 int
 main(void)
 {
+#ifdef __linux__
+    // Force GLIBC_2.35, waiting for a better solution
+    if (getenv("_probe_glibc_floor"))
+        (void)epoll_pwait2(-1, NULL, 0, NULL, NULL);
+#endif
+
     int arch[] = { PROBE_ARCH };
     int n_arch = sizeof(arch) / sizeof(arch[0]);
 
-    struct {
-        int runtime;
-        int driver;
-    } version;
+    int driver = 0;
 
-    if (cudaRuntimeGetVersion(&version.runtime) != cudaSuccess)
+    if (cuDriverGetVersion(&driver) != CUDA_SUCCESS)
         return 1;
 
-    if (cudaDriverGetVersion(&version.driver) != cudaSuccess)
+    if (cuInit(0) != CUDA_SUCCESS)
         return 2;
-
-    if (version.driver < version.runtime)
-        return 3;
 
     int count = 0;
 
-    if (cudaGetDeviceCount(&count) != cudaSuccess || !count)
-        return 4;
+    if (cuDeviceGetCount(&count) != CUDA_SUCCESS || !count)
+        return 3;
 
     int best_arch = 0;
 
     for (int i = 0; i < count; i++) {
-        struct cudaDeviceProp prop;
+        CUdevice device;
 
-        if (cudaGetDeviceProperties(&prop, i) != cudaSuccess)
+        if (cuDeviceGet(&device, i) != CUDA_SUCCESS)
             continue;
 
-        int device_arch = prop.major * 10 + prop.minor;
+        int major = 0, minor = 0;
+
+        cuDeviceGetAttribute(&major, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR, device);
+        cuDeviceGetAttribute(&minor, CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR, device);
+
+        int device_arch = major * 10 + minor;
 
         for (int j = 0; j < n_arch; j++) {
             if (arch[j] <= device_arch && arch[j] > best_arch)
@@ -46,7 +55,7 @@ main(void)
         }
     }
     if (!best_arch)
-        return 5;
+        return 4;
 
     printf("%d\n", best_arch);
     return 0;
