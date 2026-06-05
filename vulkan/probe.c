59839471d605147966350c8d5177cdf378ed4658
Adrien Gallouët <adrien@gallouet.fr>
2026-06-05 15:46:57 +0200

    Align vulkan probe with the real binary (#16)
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- vulkan/probe.c
+++ vulkan/probe.c
@@ -1,9 +1,32 @@
+#ifdef __linux__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <unistd.h>
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include <stdlib.h>
 #include <vulkan/vulkan.h>
 
 int
 main(void)
 {
+#ifdef __linux__
+    // Force GLIBC_2.27, waiting for a better solution
+    if (getenv("_probe_glibc_floor"))
+        (void)copy_file_range(-1, 0, -1, 0, 0, 0);
+#endif
+
+#ifdef _WIN32
+    // Force Windows 8, waiting for a better solution
+    if (getenv("_probe_win32_floor"))
+        (void)CreateFile2(L"", 0, 0, 0, NULL);
+#endif
+
     VkInstanceCreateInfo createInfo = {
         .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
     };
