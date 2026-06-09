ef33f7dca491c90c30af55914617101c53a480f1
Adrien Gallouët <adrien@gallouet.fr>
2026-06-09 18:50:53 +0200

    Use theRock (#21)
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- toolchains/rocm.cmake
+++ toolchains/rocm.cmake
@@ -1,7 +1,11 @@
 include("${CMAKE_CURRENT_LIST_DIR}/init.cmake")
 
-set(CMAKE_C_COMPILER   "/opt/rocm/lib/llvm/bin/clang")
-set(CMAKE_CXX_COMPILER "/opt/rocm/lib/llvm/bin/clang++")
-set(CMAKE_HIP_COMPILER "${CMAKE_CXX_COMPILER}")
+if(NOT DEFINED ROCM_PATH)
+    set(ROCM_PATH "$ENV{ROCM_PATH}")
+endif()
+
+list(APPEND CMAKE_PREFIX_PATH "${ROCM_PATH}/lib/cmake")
+set(CMAKE_C_COMPILER   "${ROCM_PATH}/lib/llvm/bin/clang")
+set(CMAKE_CXX_COMPILER "${ROCM_PATH}/lib/llvm/bin/clang++")
 
 include("${CMAKE_CURRENT_LIST_DIR}/exit.cmake")
