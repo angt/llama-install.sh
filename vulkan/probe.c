074c744268b325ea5be8e8ad0158c61b0b5a9135
Adrien Gallouët <adrien@gallouet.fr>
2026-08-01 07:02:40 +0000

    Use glibc 2.29
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- vulkan/probe.c
+++ vulkan/probe.c
@@ -2,6 +2,7 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
+#include <spawn.h>
 #include <unistd.h>
 #endif
 
@@ -16,9 +17,14 @@ int
 main(void)
 {
 #ifdef __linux__
-    // Force GLIBC_2.27, waiting for a better solution
-    if (getenv("_probe_glibc_floor"))
-        (void)copy_file_range(-1, 0, -1, 0, 0, 0);
+    // Force GLIBC_2.29, waiting for a better solution
+    if (getenv("_probe_glibc_floor")) {
+        posix_spawn_file_actions_t a;
+        if (posix_spawn_file_actions_init(&a) == 0) {
+            (void)posix_spawn_file_actions_addchdir_np(&a, "");
+            posix_spawn_file_actions_destroy(&a);
+        }
+    }
 #endif
 
 #ifdef _WIN32
